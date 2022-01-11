2a879ea3ded32ec8ad9ee1cfcbd4f32988f7beee
SchmittMichael <michael.schmitt.9@student.fhws.de>
2022-01-11 12:38:52 +0100

    Commitfix

--- frontend/src/lib/feature/listContainer/list/list.component.ts
+++ frontend/src/lib/feature/listContainer/list/list.component.ts
@@ -1,7 +1,7 @@
 import {
   Component, Input
 } from '@angular/core';
-import { List } from 'src/lib/data-access/list';
+import { List } from '../../../data-access/list';
 
 @Component({
   selector: 'list',
